dbdab9403c886b25ed567a33522d9319dc98a7e2
girishyadav <girishayadav.com>
2024-08-05 19:12:22 +0530

    first change

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 const fs = require('fs');
 
+const message = "Hello, World!";
+const fileName = "output.txt";
+
+fs.writeFile(fileName, message, (err) => {
+    if (err) {
+      console.error("An error occurred while writing to the file:", err);
+      throw err; 
+    } else {
+      console.log(`The message "${message}" was successfully written to ${fileName}`);
+    }
+  });
+
 // TODO: Write the message "Hello, World!" to the file "output.txt"
 
